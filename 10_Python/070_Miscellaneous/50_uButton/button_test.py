5c574541f7999a2d2e25e9c3868e36fc96aeab82
Francesco <francesco.ficili@gmail.com>
2022-06-24 21:08:14 +0200

    updated ubutton example

--- 10_Python/070_Miscellaneous/50_uButton/button_test.py
+++ 10_Python/070_Miscellaneous/50_uButton/button_test.py
@@ -1,15 +1,39 @@
 from ubutton import uButton
 import uasyncio as asyncio
 import machine
+from machine import Pin
+import time
 
+Relay1 = Pin(16, Pin.OUT)
+Relay2 = Pin(15, Pin.OUT)
+
+Relay1.off()
+Relay2.off()
+
+def Rel1():
+    global Relay1
+    
+    Relay1.on()
+    time.sleep(2)
+    Relay1.off()
+    print('short press')
+
+def Rel2():
+    global Relay2
+    
+    Relay2.on()
+    time.sleep(2)
+    Relay2.off()
+    print('long press')
+    
 # Instantiate the controller instance
 button = uButton(
     machine.Pin(0, machine.Pin.IN),
-    cb_short = lambda: print('short press'),
+    cb_short = Rel1,
     short_wait=True,
-    cb_long = lambda: print('long press'),
+    cb_long = Rel2,
     bounce_time=25,
-    long_time=500,
+    long_time=2000,
     act_low=True
 )
 
