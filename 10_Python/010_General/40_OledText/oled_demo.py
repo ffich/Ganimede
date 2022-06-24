0562c20e6f8f2d01f0a3a2952bd1b1dd7c52a742
Francesco <francesco.ficili@gmail.com>
2022-06-24 17:42:43 +0200

    Updated the example set of general section

--- 10_Python/010_General/40_OledText/oled_demo.py
+++ 10_Python/010_General/40_OledText/oled_demo.py
@@ -2,10 +2,15 @@ from machine import Pin, SoftI2C
 import ssd1306
 import time
 
+GanimedeE12 = 0
+GanimedeE32 = 1
+BOARD = GanimedeE12
+
 DELAY = 0.2
 
-# ESP8266 Pin assignment
-i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
+if BOARD is GanimedeE12:
+    # ESP8266 Pin assignment
+    i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
 
 oled_width = 128
 oled_height = 64
@@ -15,7 +20,8 @@ while (True):
     oled.text('Hello World!!!', 0, 0)
     oled.show()
     time.sleep(DELAY)
-    oled.text('Ganimede.E12', 0, 10)
+    if BOARD is GanimedeE12:    
+        oled.text('Ganimede.E12', 0, 10)
     oled.show()
     time.sleep(DELAY)
     oled.text('Powered by', 0, 20)
@@ -24,7 +30,7 @@ while (True):
     oled.text('microPython', 0, 30)
     oled.show()
     time.sleep(DELAY)
-    oled.text('A project by', 0, 40)
+    oled.text('A project by:', 0, 40)
     oled.show()
     time.sleep(DELAY)
     oled.text('Francesco Ficili', 0, 50)
